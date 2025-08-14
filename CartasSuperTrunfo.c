f961c224d2783d090571a9f57574b7c2b9169910
mathyeus1998 <mateus98mota@gmail.com>
2025-08-14 01:26:59 +0000

    Projeto1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,21 +2,58 @@
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Definiçao variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população.
+    char letracarta1[5], letracarta2[5];
+    int codigocidade1, codigocidade2;
+    char nome1[30], nome2[30];
+    int populacao1, populacao2;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
+    // entradas e saidas de atributos primeira carta
+
+    printf("Defina a letra para o primeira carta: ");
+    scanf("%s", &letracarta1);
+    printf("Defina código da cidade: ");
+    scanf("%d", &codigocidade1);
+    printf("Qual nome da cidade? ");
+    scanf("%s", &nome1);
+    printf("Qual a populacao da cidade de(a) %s?", &nome1);
+    scanf("%d", &populacao1);
+
+    // entradas e saidas de atributos segunda carta
+    printf("Defina a letra para o segunda carta: ");
+    scanf("%s", &letracarta2);
+    printf("Defina código da cidade: ");
+    scanf("%d", &codigocidade2);
+    printf("Qual nome da cidade? ");
+    scanf("%s", &nome2);
+    printf("Qual a populacao da cidade de(a) %s?", &nome2);
+    scanf("%d", &populacao2);
+    
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("Informações da primeira carta\n");
+    printf("Letra da carta :%s\n", letracarta1);
+    printf("Código da cidade :%d\n", codigocidade1);
+    printf("Nome da cidade :%s\n", nome1);
+    printf("Populacão da cidade :%d\n", populacao1);
+    printf("\n");
+    printf("\n");
+
+    printf("Informações da segunda carta\n");
+    printf("Letra da carta :%s\n", letracarta2);
+    printf("Código da cidade :%d\n", codigocidade2);
+    printf("Nome da cidade :%s\n", nome2);
+    printf("Populacão da cidade :%d\n", populacao2);
+
+    
+    
+    
+    
+
+    return 0;       
 }
